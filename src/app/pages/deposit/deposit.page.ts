fed2cf8de4161fbd0fe30ab54a9cc407fae3a0d3
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-03-05 21:42:51 +0100

    toast added

--- src/app/pages/deposit/deposit.page.ts
+++ src/app/pages/deposit/deposit.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InteractionService } from 'src/app/services/interaction.service';
 
 @Component({
   selector: 'app-deposit',
@@ -9,9 +10,16 @@ export class DepositPage implements OnInit {
 
   elementType: 'url' | 'canvas' | 'img' = 'url';
   value: string = 'bcrt1q6jhwfrax6c7ee5tj0g2l4r0ehppaq60dq3fyg8';
-  constructor() { }
+  constructor(private interactionService: InteractionService) { }
 
   ngOnInit() {
   }
+
+  presentToast(){
+    this.interactionService.createToast("Copied To Click Board","transparent",'center');
+  }
+
+
+
 
 }

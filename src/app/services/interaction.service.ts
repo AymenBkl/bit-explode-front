70c49eea650f6da302ef8247603f31f4f67211a7
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-04-08 14:55:17 +0200

    sweetalert 2 addeD

--- src/app/services/interaction.service.ts
+++ src/app/services/interaction.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ToastController, LoadingController, AlertController } from '@ionic/angular';
-import { observable, Observable } from 'rxjs';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Injectable({
   providedIn: 'root'
@@ -79,5 +79,92 @@ export class InteractionService {
           alert.present();
         });
     });
+  }
+
+
+  confirmBox(msg:string,text:string,icon:string,confirmBtn:string,cancelBtn:string,handler) {
+    return new Promise((resolve,reject) => {
+      Swal.fire({
+        title: msg,
+        text: text,
+        icon: icon,
+        showCancelButton: true,
+        confirmButtonText: confirmBtn,
+        cancelButtonText: cancelBtn,
+      }).then((result) => {
+        console.log(result);
+        if (result.value || result.isConfirmed) {
+          resolve({status:true});
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            resolve({status:false});
+        }
+      })
+    })
+
+  }
+
+
+  alertMsg(header:string,msg:string,type) {
+    Swal.fire(
+      header,
+      msg,
+      type
+    )
+  }
+
+  showValidationError(error) {
+    Swal.showValidationMessage(
+      `Request failed: ${error}`
+    )
+  }
+
+  private handleSnackBar(type: string): string {
+    if (type == 'error'){
+      return '#d9534f';
+    }
+    else if (type == 'success'){
+      return '#5cb85c';
+    }
+    else if (type == 'warning'){
+      return '#f0ad4e';
+    }
+    else if (type == 'info'){
+      return '';
+    }
+    else {
+      return '';
+    }
+  }
+
+  displayToast(msg:string,loading:boolean,type:string) {
+    const Toast = Swal.mixin({
+      toast: true,
+      target: '#custom-target',
+      position: 'bottom-start',
+      showConfirmButton: false,
+      className: "pos-toast-swt",
+      background:this.handleSnackBar(type),
+      didOpen: (toast) => {
+        if (loading) Swal.showLoading();
+
+      }
+    })
+
+
+    Toast.fire({
+      icon: type,
+      title: msg
+    })
+    if (!loading) {
+      setTimeout(() => {
+        Toast.close()
+      },4000);
+    }
+
+  }
+
+  closeToast() {
+    Swal.close();
+
   }
 }
